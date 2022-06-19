46ab28e9c04e35c41baa9d3fe7092e047f1f28e7
illusion <dhruvtiwari110058@gmail.com>
2022-06-19 16:18:00 +0530

    refactor

--- assets/script.jl
+++ assets/script.jl
@@ -1,17 +1,17 @@
-import Logging, Pkg; 
+import Logging, Pkg;
 
 Logging.global_logger(Logging.ConsoleLogger(stdout));
 
 if haskey(Pkg.dependencies(), "Pluto")
-    Pkg.update("Pluto");
+    Pkg.update("Pluto")
 else
-    Pkg.add("Pluto");
+    Pkg.add("Pluto")
 end
 
 using Pluto;
 
 if isempty(ARGS)
-    Pluto.run(; launch_browser=false);
+    Pluto.run(; launch_browser=false)
 else
-    Pluto.run(notebook=ARGS[1]; launch_browser=false);
+    Pluto.run(notebook=ARGS[1]; launch_browser=false)
 end
