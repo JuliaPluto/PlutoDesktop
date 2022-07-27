97f1402236918c38133d6523c598680f9fb9cc27
illusion <dhruvtiwari110058@gmail.com>
2022-07-27 21:05:06 +0530

    feat: watch user-config for changes while running

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,9 +1,28 @@
+import { app, BrowserWindow, dialog } from 'electron';
 import Store from 'electron-store';
 import fs from 'fs';
 
 const store = new Store<SettingsStore>();
 
 const userStore = new Store<UserSettingsStore>({ name: 'user-config' });
+
+userStore.onDidAnyChange(async () => {
+  const window = BrowserWindow.getFocusedWindow();
+  if (window) {
+    const { response } = await dialog.showMessageBox(window, {
+      title: 'Restart needed!',
+      message:
+        'Looks like user-config.json has been changed, Application needs to restart in order to apply these changes',
+      buttons: ['Restart now', 'Apply changes later'],
+      cancelId: 1,
+      noLink: true,
+    });
+    if (response === 0) {
+      app.relaunch();
+      app.exit();
+    }
+  }
+});
 
 const openUserStoreInEditor = () => {
   if (!fs.existsSync(userStore.path)) fs.writeFileSync(userStore.path, '{}');
