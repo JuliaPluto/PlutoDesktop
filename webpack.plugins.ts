2a20ade9cbff35791c432482ca42e3e0ee71d243
Fons van der Plas <fonsvdplas@gmail.com>
2025-12-16 11:55:09 +0100

    update packaeg versions

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,6 +1,5 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 export const plugins = [
