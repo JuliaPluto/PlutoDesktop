501b01d7be12ee2ee624546e1329154e5a9dee12
Boshra Ariguib <bosh@s-8d3a3ee3.on.site.uni-stuttgart.de>
2026-02-09 10:38:53 +0100

    fixed absolute paths

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,30 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+import * as path from 'path';
+
+// import { PlutoExport } from '../../types/enums';
+
+// const __filename = process.argv[1];
+// const __dirname = path.dirname(process.argv[1]);
+
+export type Channels = 'ipc-example';
+
+contextBridge.exposeInMainWorld('plutoDesktop', {
+  ipcRenderer: {
+    sendMessage(channel: Channels, args: unknown[]) {
+      ipcRenderer.send(channel, args);
+    },
+    on(channel: Channels, func: (...args: unknown[]) => void) {
+      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+        func(...args);
+      ipcRenderer.on(channel, subscription);
+
+      return () => ipcRenderer.removeListener(channel, subscription);
+    },
+    once(channel: Channels, func: (...args: unknown[]) => void) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+  },
+});
--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -3,12 +3,18 @@ import type { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const mainConfig: Configuration = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/index.ts',
+  entry: path.join(__dirname, 'src/index.ts'),
   // Put your normal webpack config below here
   module: {
     rules,
