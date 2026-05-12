36d715138da775288d36a4c8eaac48e7d0888b06
Fons van der Plas <fonsvdplas@gmail.com>
2026-05-12 17:12:50 +0200

    Delete files from before electron forge

--- old/bundler/scripts/delete-source-maps.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import * as rimraf from 'rimraf';
-import fs from 'node:fs';
-import webpackPaths from '../configs/webpack.paths.ts';
-
-export default function deleteSourceMaps() {
-  rimraf.sync(path.join(webpackPaths.distMainPath, '*.js.map'), { glob: true });
-}
